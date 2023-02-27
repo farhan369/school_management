be5601cf6f9935ceef47a93fb3b8f0568a5caed4
farhan.fm <farhan.fm@cied.in>
2023-02-27 10:08:20 +0530

    migration files

---
+++ account/constants.py
@@ -0,0 +1,11 @@
+STUDENT = 1
+TEACHER = 2
+ADMIN   = 3
+
+
+
+USER_TYPE_CHOICES = (
+      (STUDENT, 'student'),
+      (TEACHER, 'teacher'),
+      (ADMIN, 'admin'),
+  )
---
+++ event/constants.py
@@ -0,0 +1,6 @@
+TIME = 0
+DISTANCE = 1
+EVENT_TYPE_CHOICE = (
+        (TIME,'Time'),
+        (DISTANCE,'Distance'),
+    )
